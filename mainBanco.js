27890008cf97525425f86f69edc977836d7c4e08
marcy404 <oliveira.oquendo01@gmail.com>
2022-10-05 17:25:14 -0300

    Entrega Final

--- mainBanco.js
+++ mainBanco.js
@@ -14,14 +14,15 @@ cont = 5;
 
 cont2 = 20;
 
+
 function randomNum(x, y) {
   return Math.random() * x + y;
 }
 
-function descript(a, b, c) {
-  if (b > c) {
+function descript(saldo, valorTransf, limiteTransf) {
+  if (valorTransf > limiteTransf) {
     return "Limite atingido";
-  } else if (a < b) {
+  } else if (saldo < valorTransf) {
     return "Saldo insuficiente";
   } else {
     return "Aprovado";
@@ -33,7 +34,7 @@ while (cont > 0) {
     Número: Math.floor(randomNum(100, 1)),
     Saldo: randomNum(10000, 0).toFixed(2),
     LimiteTransf: randomNum(50000, 1000).toFixed(2),
-    Histórico: "",
+    Historico: "",
     Banco: bank[Math.floor(randomNum(3, 0))],
     TransfRecebidas: 0,
     TransfEfetuadas: 0,
@@ -43,19 +44,28 @@ while (cont > 0) {
   table.push(conta);
   nums.push(conta.Número);
 }
-
+console.log(table)
 while (cont2 > 0) {
-  let transferência = {
+  let contaOrigem = table[nums[Math.floor(randomNum(5, 0))]];
+  let contaDestino = table[nums[Math.floor(randomNum(5, 0))]];
+  if (contaOrigem == contaDestino) {
+    continue
+  }
+
+  let transferencia = {
     Meio: metod[Math.floor(randomNum(3, 0))],
     Tipo: type[Math.floor(randomNum(2, 0))],
     Valor: randomNum(50000, 1000).toFixed(2),
-    Origem: nums[Math.floor(randomNum(5, 0))],
-    Destino: nums[Math.floor(randomNum(5, 0))],
-    Descrição: descript(conta.Saldo, transferência.Valor, conta.LimiteTransf),
+    Origem: contaOrigem,
+    Destino: contaDestino
   };
+
+  transferencia.Descrição = descript(transferencia.Origem.Saldo, transferencia.Valor, transferencia.Origem.LimiteTransf)
   cont2--;
-  transf.push(transferência);
+  
+  console.log(transferencia)
+  transf.push(transferencia);
 }
 
 console.table(table);
-console.table(transferência);
+console.table(transf);
